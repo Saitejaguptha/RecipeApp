531599adbac7d69532da77bb0cb7716a58418ae2
Saitejaguptha <saitejguptha111@gmail.com>
2024-11-15 21:46:11 +0530

    Inital Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '93a64df9-9559-47ac-8f49-48cc97b26408';
+export const MODAL_CLOSE_SEC = 2.5;
